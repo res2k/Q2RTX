ffab86a3e5f800c30d22a103e72e300ce15492c0
Frank Richter <frank.richter@gmail.com>
2021-03-26 13:07:06 +0100

    Add check for 'native' vs x86 game library

--- src/client/compat_server.c
+++ src/client/compat_server.c
@@ -33,22 +33,103 @@ void SV_Init(void);
 void SV_Shutdown(const char *finalmsg, error_type_t type);
 unsigned SV_Frame(unsigned msec);
 
+#if (defined _WIN64)
+#define ENABLE_SERVER_PROCESS
+#endif
+
+#if defined(ENABLE_SERVER_PROCESS)
+static bool using_compat_server;
+
+extern cvar_t *fs_game;
+extern cvar_t *sys_forcegamelib;
+extern cvar_t *sys_libdir;
+
+// Check whether a specific game library exists
+static bool game_library_exists(const char* game, const char* prefix, const char* cpu_str)
+{
+    char path[MAX_OSPATH];
+    size_t len;
+
+    len = Q_concat(path, sizeof(path), sys_libdir->string,
+                   PATH_SEP_STRING, game, PATH_SEP_STRING,
+                   prefix, "game", cpu_str, LIBSUFFIX, NULL);
+    if (len >= sizeof(path)) {
+        return false;
+    }
+
+    return os_access(path, F_OK) == 0;
+}
+
+// Check whether a supported game library exists for the given CPU
+static bool have_cpu_gamelib(const char* game, const char* cpu_str)
+{
+    return game_library_exists(game, "q2pro_", cpu_str) || game_library_exists(game, "", cpu_str);
+}
+#endif
+
 void SV_ErrorEvent_InClient(netadr_t *from, int ee_errno, int ee_info)
 {
+    // FIXME: Overriding this isn't actually needed?
     SV_ErrorEvent(from, ee_errno, ee_info);
 }
 
 void SV_Init_InClient(void)
 {
+#if !defined(ENABLE_SERVER_PROCESS)
     SV_Init();
+#else
+    bool have_native_gamelib = false;
+    bool have_x86_gamelib = false;
+    const char *game_str = fs_game->string[0] ? fs_game->string : BASEGAME;
+
+    // Note: This should check the same game library names as SV_InitGameProgs!
+    if (sys_forcegamelib->string[0] && (os_access(sys_forcegamelib->string, F_OK) == 0))
+    {
+        have_native_gamelib = true;
+    }
+    else
+    {
+        have_native_gamelib = have_cpu_gamelib(game_str, CPUSTRING);
+    }
+    if(!have_native_gamelib)
+    {
+        have_x86_gamelib = have_cpu_gamelib(game_str, "x86");
+    }
+
+    /* Default logic if we have a native gamelib, or none at all.
+     * (Will generate an error message in the latter case.) */
+    if (have_native_gamelib || (!have_native_gamelib && !have_x86_gamelib))
+    {
+        SV_Init();
+        return;
+    }
+
+    // We don't have a native gamelib, but an x86 one we can start a server for!
+    using_compat_server = true;
+    // TODO: Start separate server process
+    // TODO: Other sensible things to keep up appearances?
+#endif
 }
 
 void SV_Shutdown_InClient(const char *finalmsg, error_type_t type)
 {
+#if !defined(ENABLE_SERVER_PROCESS)
     SV_Shutdown(finalmsg, type);
+#else
+    if(!using_compat_server)
+        SV_Shutdown(finalmsg, type);
+    using_compat_server = false;
+#endif
 }
 
 unsigned SV_Frame_InClient(unsigned msec)
 {
+#if !defined(ENABLE_SERVER_PROCESS)
     return SV_Frame(msec);
+#else
+    if(!using_compat_server)
+        return SV_Frame(msec);
+
+    return UINT_MAX; // force CL_Frame() result to have precedence
+#endif
 }
